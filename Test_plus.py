5dd77e247821da38f77ea747b8ff2ab1add58851
Fares Hany Mohamed <F.Hany2246@nu.edu.eg>
2024-11-10 16:35:45 +0200

    Update Test_plus.py

--- Test_plus.py
+++ Test_plus.py
@@ -1,16 +1,15 @@
 import unittest
 from plus import *
 
-
 class Testplus(unittest.TestCase):
     def test_plus_positive_numbers(self):
         result = plus(2, 3)
         self.assertEqual(result, 5)
-
+        
     def test_plus_negative_numbers(self):
         result = plus(-2, -3)
         self.assertEqual(result, -5)
-
+        
     def test_add_zero(self):
         result = plus(0, 5)
         self.assertEqual(result, 5)
@@ -18,6 +17,6 @@ class Testplus(unittest.TestCase):
     def test_add_float_numbers(self):
         result = plus(2.5, 3.7)
         self.assertEqual(result, 6.2)
-
+        
 if _name_ == '_main_':
-    unittest.main()
+    unittest.main()
